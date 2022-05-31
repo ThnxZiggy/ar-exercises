46663cd9d8196a81d7a2d9dad1ad800d762a748d
Manmohit Matharu <manmohit.matharu@gmail.com>
2022-05-31 00:49:33 -0400

    complete exercise 6

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,16 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+#total revenue for all stores
+@total_revenue = Store.sum(:annual_revenue)
+pp @total_revenue
+
+#average revenue across all stores
+@average_revenue = @total_revenue/Store.count
+puts @average_revenue
+
+#number of stores with revenue > $1m
+@rich_stores = Store.where('annual_revenue > ?', 1000000).count
+
+puts @rich_stores
