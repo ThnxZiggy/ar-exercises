458d4a7d4cdd2810a0bf579d05c041ac17738faa
Manmohit Matharu <manmohit.matharu@gmail.com>
2022-05-31 00:09:05 -0400

    completed first three exercises

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,7 +1,18 @@
 require_relative '../setup'
 require_relative './exercise_1'
+require 'pp'
 
 puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+burnaby = Store.find_by(id: 1)
+
+@store1 = burnaby
+
+@store2 = Store.find_by(id: 2)
+
+@store1.update(name: 'Burnaby Updated')
+pp @store1
+pp @store2
